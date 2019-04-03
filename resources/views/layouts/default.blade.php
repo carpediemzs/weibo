387013ea73021cf2f36a2c235460364eec9de061
sailorzs <sailorzs@126.com>
2019-04-03 12:24:15 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  @yield('content')
+
+</body>
+
+
+
+</html>
