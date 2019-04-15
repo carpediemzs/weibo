69219a5f391e8b537995e4961dc3b259c37c38b4
sailorzs <sailorzs@126.com>
2019-04-15 17:26:26 +0000

    重置密码

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -21,8 +21,16 @@
             </div>
 
             <div class="form-group">
-              <label for="password">密码：</label>
+              <label for="password">密码：(<a href="{{route('password.request')}}">忘记密码</a>)</label>
               <input type="password" name="password" id="password" class="form-control" value="{{old('password')}}">
+            </div>
+
+            <div class="form-group">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+                <label for="exampleCheck1" class="form-check-label">记住我</label>
+
+              </div>
             </div>
 
             <button type="submit" class="btn btn-primary">登录</button>
